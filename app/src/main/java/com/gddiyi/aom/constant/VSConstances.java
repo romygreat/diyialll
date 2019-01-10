b97c8136b5e3c005931de0bb56a198ca9da7490f
romygreat <907264506@qq.com>
2019-01-10 16:08:53 +0800

    修改代码DTO

---
+++ app/src/main/java/com/gddiyi/aom/constant/VSConstances.java
@@ -0,0 +1,37 @@
+package com.gddiyi.aom.constant;
+
+public class VSConstances {
+//      "id": 31,
+//              "title": "手动阀手动阀2",
+//              "path": "ad/9e810023b8e689be648c657cf50d5c43.mp4",
+//              "type": "video/mp4",
+//              "prov": 440000,
+//              "city": 441300,
+//              "zone": 441303,
+//              "shop_id": 62,
+//              "terminal_type": 1,
+//              "sort": 222,
+//              "update_time": "1970-01-01 08:32:50",
+//              "delete_time": 0,
+//              "create_time": "1970-01-01 08:32:50",
+//              "start_time": 1546843624,
+//              "end_time": 1546930209,
+//              "status": 1
+ public static final int id=0;
+ public static final int title=1;
+ public static final int path=2;
+ public static final int type=3;
+ public static final int prov=4;
+ public static final int city=5;
+ public static final int zone=6;
+ public static final int shop_id=7;
+ public static final int terminal_type=8;
+ public static final int sort=9;
+ public static final int update_time=10;
+ public static final int delete_time=11;
+ public static final int create_time=12;
+ public static final int start_time=13;
+ public static final int end_time=14;
+ public static final int status=15;
+
+}
