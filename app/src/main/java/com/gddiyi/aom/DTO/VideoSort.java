2178741063fd8eabb51a80d209a3e39e4aff63c4
romygreat <907264506@qq.com>
2019-01-10 16:10:02 +0800

    修改代码DTO

---
+++ app/src/main/java/com/gddiyi/aom/DTO/VideoSort.java
@@ -0,0 +1,32 @@
+package com.gddiyi.aom.DTO;
+
+class VideoSort {
+    String desc;
+    String id;
+    String shop_id;
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getShop_id() {
+        return shop_id;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setShop_id(String shop_id) {
+        this.shop_id = shop_id;
+    }
+
+}
